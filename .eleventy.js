3ceb37235a59bf4bcf110fe6846dfb1fd5b4f591
wbglaeser <ben.glaeser@tuta.io>
2023-12-02 13:52:44 +0100

    fix root path

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
+  // Default path for local development
+  let rootPath = "/";
+
+  // If environment is production, change the path
+  if(process.env.ELEVENTY_ENV === "production") {
+    rootPath = "/ckg-site/";
+  }
+
+  // Pass the stylesheet path to your templates
+  eleventyConfig.addShortcode("rootPath", function() {
+    return rootPath;
+  });
+
   return {
     passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
